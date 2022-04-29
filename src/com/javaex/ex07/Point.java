3765efe19cff6e63813dadb5c7b93a814d019a71
최정필 <wjdvlf1407@gmail.com>
2022-04-29 21:50:49 +0900

    Ex07 문제풀기

--- src/com/javaex/ex07/Point.java
+++ src/com/javaex/ex07/Point.java
@@ -1,16 +1,26 @@
 package com.javaex.ex07;
 
 public class Point {
-	
+
 	private int x;
 	private int y;
-	
+
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
-	//equals() 메소드를 작성하세요.
+	// equals() 메소드를 작성하세요.
+	public boolean equals(Object obj) {
+		Point p = (Point) obj;
 
-	
+		if (this.x == p.x) {		//x만 같으면 된다.
+			return true;
+		} else {
+			return false;
+
+		}
+
+	}
+
 }
