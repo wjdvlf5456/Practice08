b9e0a3bdf36ed3102c797855831740dfbdab2399
최정필 <wjdvlf1407@gmail.com>
2022-04-29 21:55:34 +0900

    Ex06 문제풀기

--- src/com/javaex/ex06/Point.java
+++ src/com/javaex/ex06/Point.java
@@ -9,6 +9,24 @@ public class Point {
 		this.x = x;
 		this.y = y;
 	}
+	
+	//equals 메소드가 없는데도 출력이 되는 이유는 모든 클래스는 Object라는 최상위클래스에 상속되어 있어서이다.
+	//==는 주소값을 비교하여 출력이 되고 equals는 x와 y의 값이 완전 같고 p4와 p1도 같으니 true로 출력될 거 같다.
+	//예상
+	
+	//true
+	//false
+	//false
+	//true
+	//true
+	//true
+	
+	//정답
+	//equals라고 재정의한 값이 없어 출력이 안 
+	//true
+	//false
+	//false
+	//true
 
 	
 	
