a06eeb1a2b504256ac897148c58ca1be03a23a27
최정필 <wjdvlf1407@gmail.com>
2022-04-30 18:34:32 +0900

    Ex04 푸는 중

--- src/com/javaex/ex04/SumApp.java
+++ src/com/javaex/ex04/SumApp.java
@@ -6,14 +6,33 @@ public class SumApp {
 
 	public static void main(String[] args) {
 
-		int sum = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("숫자를 공백으로 구분해서 입력하세요");
-		String numLine = sc.nextLine();
 
 		// 코드를 작성하세요
-		sum = sum + Integer.parseInt(numLine);
-		System.out.println("합:" + sum);
+		/*int sum = 0;
+		while (true) {
+		
+			String numLine = sc.next();
+			sum = sum + Integer.parseInt(numLine);
+		
+			if (numLine.equals("")) {
+				break;
+			}
+			System.out.println("합:" + sum);
+		
+		}
+		System.out.println("외곽: " + sum);
+		*/
+
+		int sum = 0;
+		do {
+
+			String numLine = sc.next();
+			sum += Integer.parseInt(numLine);
+
+		} while (sc.next().equals(null));
+		System.out.println(sum);
 
 		sc.close();
 	}
