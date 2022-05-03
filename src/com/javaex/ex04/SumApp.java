2960ba23b8dd2aa7523bdcc70bb5cd75b42289cf
최정필 <wjdvlf1407@gmail.com>
2022-05-03 17:40:55 +0900

    Ex04 공백으로 구분해 합계구하기

--- src/com/javaex/ex04/SumApp.java
+++ src/com/javaex/ex04/SumApp.java
@@ -10,14 +10,15 @@ public class SumApp {
 		System.out.println("숫자를 공백으로 구분해서 입력하세요");
 
 		// 코드를 작성하세요
+		String numLine = sc.nextLine();
+		String[] number = numLine.split(" ");
+
 		int sum = 0;
-
-		while (sc.hasNext()) {
-
-			String numLine = sc.next();
-			sum = sum + Integer.parseInt(numLine);
+		for (int i = 0; i < number.length; i++) {
+			sum = sum + Integer.parseInt(number[i]);
 
 		}
+
 		System.out.println("합:" + sum);
 
 		sc.close();
